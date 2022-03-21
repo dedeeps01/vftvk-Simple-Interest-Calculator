c0d258023c05e767b92ec22b06b5dc28d868b4b1
dedeeps01 <69516049+dedeeps01@users.noreply.github.com>
2022-03-21 10:58:05 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // computes interest
 function compute()
 {
+    check();
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
